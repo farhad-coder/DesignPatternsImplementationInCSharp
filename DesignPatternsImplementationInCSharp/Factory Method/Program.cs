1c732a5ccbb94848e7039891597e72757b547c91
Farhad <farhad.ahmadi.dev@gmail.com>
2024-12-21 03:06:32 +0330

    Factory Method

---
+++ DesignPatternsImplementationInCSharp/Factory Method/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Factory_Method;
+
+AnimalFactory dogFactory = new DogFactory();
+IAnimal dog = dogFactory.CreateAnimal();
+dog.Speak();
+
+AnimalFactory catFactory = new CatFactory();
+IAnimal cat = catFactory.CreateAnimal();
+cat.Speak();
