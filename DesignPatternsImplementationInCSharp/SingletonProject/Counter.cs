398636d82c2e6fb30331566b1d71dece9fe2ec08
Farhad <farhad.ahmadi.dev@gmail.com>
2024-12-21 02:42:54 +0330

    Singleton

--- DesignPatternsImplementationInCSharp/SingletonProject/Counter.cs
+++ DesignPatternsImplementationInCSharp/SingletonProject/Counter.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace SingletonProject
 {
-    internal class Counter
+    public class Counter
     {
+        private static Counter _instance;
+
+        private static readonly Object _lock = new Object();
+
+        private int _count;
+        private Counter()
+        {
+            _count = 0;
+        }
+
+        public static Counter Instance
+        {
+            get
+            {
+
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Counter();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public void Increment()
+        {
+            _count++;
+        }
+
+        public int GetCount()
+        {
+            return _count;
+        }
     }
 }
--- DesignPatternsImplementationInCSharp/SingletonProject/Program.cs
+++ DesignPatternsImplementationInCSharp/SingletonProject/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Salam Donya!");
+﻿
+using SingletonProject;
+
+Counter counter1 = Counter.Instance;
+counter1.Increment();
+counter1.Increment();
+Console.WriteLine("Counter1: " + counter1.GetCount());
+
+Counter counter2 = Counter.Instance;
+counter2.Increment();
+Console.WriteLine("Counter2: " + counter2.GetCount());
+
+Console.WriteLine(Object.ReferenceEquals(counter1,counter2));
+
