3bc59775c0e733adb050df2d872cd630f466fe97
Farhad <farhad.ahmadi.dev@gmail.com>
2024-12-21 14:34:27 +0330

    Prototype

---
+++ DesignPatternsImplementationInCSharp/PrototypeProject/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeProject
+{
+    public class Car : ICarPrototype
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int EngineCapacity { get; set; }
+
+        public Car(string model, string color, int engineCapacity)
+        {
+            Model = model;
+            Color = color;
+            EngineCapacity = engineCapacity;
+        }
+        public ICarPrototype Clone()
+        {
+            return (ICarPrototype)MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return $"Model: {Model}, Color: {Color}, Engine Capacity: {EngineCapacity} cc";
+        }
+    }
+}
---
+++ DesignPatternsImplementationInCSharp/PrototypeProject/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using PrototypeProject;
+
+Car originalCar = new Car("Tesla Model S", "Red", 1000);
+
+Car clonedCar = (Car)originalCar.Clone();
+
+Console.WriteLine("Original Car: " + originalCar);
+Console.WriteLine("Cloned Car: " + clonedCar);
+
+clonedCar.Color = "Blue";
+Console.WriteLine("After modifying the cloned car's color:");
+
+Console.WriteLine("Original Car: " + originalCar);
+Console.WriteLine("Cloned Car: " + clonedCar);
