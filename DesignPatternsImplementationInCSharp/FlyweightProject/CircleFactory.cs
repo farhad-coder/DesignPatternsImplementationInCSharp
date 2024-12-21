4608fbd144495e663fba254b962a50a720cb72a3
Farhad <farhad.ahmadi.dev@gmail.com>
2024-12-21 22:39:48 +0330

    Flyweight

---
+++ DesignPatternsImplementationInCSharp/FlyweightProject/CircleFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightProject
+{
+    public class CircleFactory
+    {
+        private static Dictionary<string, Circle> _circleMap = new Dictionary<string, Circle>();
+
+        private static string[] colors = new string[] { "Red", "Green", "Blue", "Yellow", "Black", "White" };
+
+        public static Circle GetCircle(string color)
+        {
+            Circle circle = null;
+            if (_circleMap.ContainsKey(color))
+            {
+                circle = _circleMap[color];
+            }
+            else
+            {
+                circle = new Circle(color);
+                _circleMap[color] = circle;
+                Console.WriteLine($"Creating circle of color : {color}");
+            }
+            return circle;
+        }
+
+        public static string GetRandomColor()
+        {
+            Random random = new Random();
+            return colors[random.Next(colors.Length)];
+        }
+
+        public static int GetRandomX()
+        {
+            Random random = new Random();
+            return random.Next(100);
+        }
+
+        public static int GetRandomY()
+        {
+            Random random = new Random();
+            return random.Next(100);
+        }
+    }
+}
---
+++ DesignPatternsImplementationInCSharp/FlyweightProject/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using FlyweightProject;
+
+for (int i = 0; i < 20; i++)
+{
+    Circle circle = CircleFactory.GetCircle(CircleFactory.GetRandomColor());
+    circle.SetX(CircleFactory.GetRandomX());
+    circle.SetY(CircleFactory.GetRandomY());
+    circle.SetRadius(100);
+    circle.Draw();
+}
