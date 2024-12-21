ae0d4d4931069bc5e8d04635b702f0a03525e12a
Farhad <farhad.ahmadi.dev@gmail.com>
2024-12-21 13:22:37 +0330

    FactoryMethod

---
+++ DesignPatternsImplementationInCSharp/FactoryMethodProject/DogFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodProject
+{
+    public class DogFactory : AnimalFactory
+    {
+        public override IAnimal CreateAnimal()
+        {
+            return new Dog();
+        }
+    }
+}
---
+++ DesignPatternsImplementationInCSharp/FactoryMethodProject/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using FactoryMethodProject;
+
+AnimalFactory dogFactory = new DogFactory();
+IAnimal dog = dogFactory.CreateAnimal();
+dog.Speak();
+
+AnimalFactory catFactory = new CatFactory();
+IAnimal cat = catFactory.CreateAnimal();
+cat.Speak();
