cbde4dcae72089b7febb2eaeaeabb3514d5d1022
Farhad <farhad.ahmadi.dev@gmail.com>
2024-12-21 20:21:43 +0330

    Facade

---
+++ DesignPatternsImplementationInCSharp/FacadeProject/OnlineShoppingFacade.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeProject
+{
+    public class OnlineShoppingFacade
+    {
+        private ProductCatalog _productCatalog;
+        private OrderSystem _orderSystem;
+        private PaymentSystem _paymentSystem;
+
+        public OnlineShoppingFacade()
+        {
+            _productCatalog = new ProductCatalog();
+            _orderSystem = new OrderSystem();
+            _paymentSystem = new PaymentSystem();
+        }
+
+        public void PurchaseProduct()
+        {
+            _productCatalog.GetProducts();
+            _orderSystem.PlaceOrder();
+            _paymentSystem.ProcessPayment();
+            Console.WriteLine("Product Purchased Successfully!");
+        }
+    }
+}
---
+++ DesignPatternsImplementationInCSharp/FacadeProject/ProductCatalog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeProject
+{
+    public class ProductCatalog
+    {
+        public void GetProducts()
+        {
+            Console.WriteLine("Fetching products from the catalog...");
+        }
+    }
+}
