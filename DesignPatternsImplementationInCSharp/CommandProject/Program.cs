bcbf2e3407e0d5d11d0aaa88c94b92448f007ceb
Farhad <farhad.ahmadi.dev@gmail.com>
2024-12-22 17:13:57 +0330

    Command

---
+++ DesignPatternsImplementationInCSharp/CommandProject/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using CommandProject;
+
+Light livingRoomLight = new Light();
+Television livingRoomTv = new Television();
+
+ICommand lightOn = new LightOnCommand(livingRoomLight);
+ICommand lightOff = new LightOffCommand(livingRoomLight);
+ICommand tvOn = new TelevisionOnCommand(livingRoomTv);
+ICommand tvOff = new TelevisionOffCommand(livingRoomTv);
+
+RemoteControl remote = new RemoteControl();
+
+remote.Submit(lightOn);
+remote.Submit(tvOn);
+remote.Undo();
+remote.Submit(lightOff);
+remote.Undo();
---
+++ DesignPatternsImplementationInCSharp/CommandProject/Television.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandProject
+{
+    public class Television
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Television is ON");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Television is OFF");
+        }
+    }
+}
