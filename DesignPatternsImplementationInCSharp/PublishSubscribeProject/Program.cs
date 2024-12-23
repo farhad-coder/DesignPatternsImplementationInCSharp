cbda07e9e3e01412074f0770f1d9896bd944aff3
Farhad <farhad.ahmadi.dev@gmail.com>
2024-12-23 23:34:49 +0330

    PublishSubscribe

---
+++ DesignPatternsImplementationInCSharp/PublishSubscribeProject/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using PublishSubscribeProject;
+
+Publisher publisher = new Publisher();
+
+Subscriber subscriber1 = new Subscriber("Subscriber 1");
+Subscriber subscriber2 = new Subscriber("Subscriber 2");
+
+publisher.DataPublished += subscriber1.OnDataReceived;
+publisher.DataPublished += subscriber2.OnDataReceived;
+
+publisher.Publish("First Message");
+publisher.Publish("Second Message");
+
+publisher.DataPublished -= subscriber1.OnDataReceived;
+
+publisher.Publish("Third Message");
+
