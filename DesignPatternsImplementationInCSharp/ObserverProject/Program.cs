54835251d4e3a36cb6899208c830a6768fb253ec
Farhad <farhad.ahmadi.dev@gmail.com>
2024-12-23 21:02:54 +0330

    Observer

---
+++ DesignPatternsImplementationInCSharp/ObserverProject/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using ObserverProject;
+
+ConcreteSubject subject = new ConcreteSubject();
+
+ConcreteObserver observer1 = new ConcreteObserver("Observer 1");
+ConcreteObserver observer2 = new ConcreteObserver("Observer 2");
+
+subject.Attach(observer1);
+subject.Attach(observer2);
+
+subject.SubjectState = "State 1";
+subject.SubjectState = "State 2";
+
+subject.Detach(observer1);
+
+subject.SubjectState = "State 3";
+
+Console.ReadLine();
