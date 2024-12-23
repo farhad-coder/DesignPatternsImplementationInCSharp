00e43b462b1e6d9311419024aaab6a53c79340e2
Farhad <farhad.ahmadi.dev@gmail.com>
2024-12-23 22:08:42 +0330

    Strategy

---
+++ DesignPatternsImplementationInCSharp/StrategyProject/BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyProject
+{
+    public class BubbleSort : ISortStrategy
+    {
+        public void Sort(List<int> list)
+        {
+            Console.WriteLine("Sorting using Bubble Sort");
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = 0; j < list.Count - i - 1; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DesignPatternsImplementationInCSharp/StrategyProject/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using StrategyProject;
+
+List<int> numbers = new List<int> { 9, 8, 4, 6, 5, 7 };
+
+SortContext context = new SortContext();
+
+context.SetSortStrategy(new BubbleSort());
+context.Sort(numbers);
+
+context.SetSortStrategy(new QuickSort());
+context.Sort(numbers);
+
+context.SetSortStrategy(new InsertionSort());
+context.Sort(numbers);
